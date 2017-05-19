f38ed070079c905dc8c77e4801395b329717ae5a
Matt Bowytz <mbowytz@gmail.com>
2017-05-19 00:54:47 -0400

    work for exercises

--- responder_search/js/app.js
+++ responder_search/js/app.js
@@ -62,15 +62,98 @@ var App = (function() {
     var rows = '';
     for (var i in _results) {
       var responder = _results[i];
-      rows += '<tr>' + 
-                '<td>' + responder.name + '</td>' + 
-                '<td>' + responder.occupation + '</td>' + 
-                '<td>' + responder.city + ', ' + responder.state + '</td>' + 
-                '<td>' + responder.status + '</td>' + 
-              '</tr>'; 
+      rows += '<tr>' +
+                '<td>' + responder.name + '</td>' +
+                '<td>' + responder.occupation + '</td>' +
+                '<td>' + responder.city + ', ' + responder.state + '</td>' +
+                '<td>' + responder.status + '</td>' +
+              '</tr>';
     }
-    
-    $('#responder_results tbody').html(rows);
+
+    var _rEscapeChars = /\/|\\|\.|\||\*|\&|\+|\(|\)|\[|\]|\?|\$|\^/g;
+    var _rMatch = /[A-Z]+[a-z]+|[0-9]+/g;
+    var _length = _results.length;
+    var result = $('#responder_results tbody');
+    var search = $('.search-query');
+    var occfilter = $('#occupation-filter');
+    var avfilter = $('#availability-filter');
+
+    result.html(rows);
+
+    search.on("keyup", function () {
+      var value = search.val().replace(_rEscapeChars, "");
+      var regex = new RegExp(value, "i");
+      var matches = [];
+      if (value) {
+        for (var i = _length; i--;) {
+          if (regex.test(_results[i].name)) {
+            matches.push(_results[i]);
+          } else {
+            result.html('No matches, please try again.');
+          }
+        }
+        if (matches.length) {
+          var newrows = '';
+          for (var i = matches.length; i--;) {
+            newrows += '<tr>' +
+                        '<td>' + matches[i].name + '</td>' +
+                        '<td>' + matches[i].occupation + '</td>' +
+                        '<td>' + matches[i].city + ', ' + matches[i].state + '</td>' +
+                        '<td>' + matches[i].status + '</td>' +
+                      '</tr>'
+          }
+          result.html(newrows);
+        }
+      } else {
+        result.html(rows);
+      }
+    });
+
+    occfilter.on('change', function() {
+      var occ = $.grep(_results, function(el, i) {
+        if (el.occupation === occfilter.val()) {
+          return el;
+        }
+      });
+      var occrows = '';
+      for (var i = occ.length; i--;) {
+        occrows += '<tr>' +
+                    '<td>' + occ[i].name + '</td>' +
+                    '<td>' + occ[i].occupation + '</td>' +
+                    '<td>' + occ[i].city + ', ' + occ[i].state + '</td>' +
+                    '<td>' + occ[i].status + '</td>' +
+                  '</tr>'
+      }
+
+      if (occrows.length > 0) {
+        result.html(occrows);
+      } else {
+        result.html(rows);
+      }
+    });
+
+    avfilter.on('change', function() {
+      var av = $.grep(_results, function(el, i) {
+        if (el.status === avfilter.val()) {
+          return el;
+        }
+      });
+      var avrows = '';
+      for (var i = av.length; i--;) {
+        avrows +=  '<tr>' +
+                  '<td>' + av[i].name + '</td>' +
+                  '<td>' + av[i].occupation + '</td>' +
+                  '<td>' + av[i].city + ', ' + av[i].state + '</td>' +
+                  '<td>' + av[i].status + '</td>' +
+                '</tr>'
+      }
+
+      if (avrows.length > 0) {
+        result.html(avrows);
+      } else {
+        result.html(rows);
+      }
+    });
   }
 
   /**
